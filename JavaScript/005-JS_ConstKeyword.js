c899515edeaa281c2c9b107b9cba657b58d080e7
tineshwar_singh <tineshwar.singh123@gmail.com>
2023-05-14 18:04:21 +0530

    JS_ConstKeyword added

---
+++ JavaScript/005-JS_ConstKeyword.js
@@ -0,0 +1,18 @@
+//? const keyword 
+//* declare variable with let keyword 
+
+//* --> The const keyword was introduced in ES6 (2015).
+//* --> Variables defined with const cannot be Redeclared.
+//* --> Variables defined with const cannot be Reassigned.
+//* --> Variables defined with const have Block Scope.
+
+console.log("--- 005-JS_ConstKeyword.js ---");
+
+const PI = 3.14;
+console.log(PI);
+
+
+//* const PI = 3.14;
+//* PI = 3.15;
+//* console.log(PI);
+//! caught TypeError: Assignment to constant variable.

e37530866cdb2ea31b55ff25bed5707ff7e5afa3
tineshwar_singh <tineshwar.singh123@gmail.com>
2023-05-14 16:17:49 +0530

    JS_LetKeyword added

---
+++ JavaScript/004-JS_LetKeyword.js
@@ -0,0 +1,29 @@
+//? let keyword 
+//* declare variable with let keyword 
+
+//* --> The let keyword was introduced in ES6 (2015).
+//* --> Variables defined with let cannot be Redeclared.
+//* --> Variables defined with let must be Declared before use.
+//* --> Variables defined with let have Block Scope.
+
+console.log("--- 004-JS_LetKeyword.js ---");
+
+let myFirstName = "Tineshwar";
+console.log(myFirstName);
+
+
+//! let x = "John Doe";
+//! let x = 0;
+//! SyntaxError: 'x' has already been declared.
+
+
+//* console.log(firstNameLet_1);
+//* let firstNameLet_1 = "Tineshwar";
+//* firstNameLet_1 = "Raja";
+//! Error - Uncaught ReferenceError: Cannot access 'firstNameLet_1' before initialization
+
+
+//* let firstNameLet_2 = "Tineshwar";
+//* let firstNameLet_2 = "Raja";
+//! Error - Uncaught SyntaxError: Identifier 'firstNameLet_2' has already been declared
+

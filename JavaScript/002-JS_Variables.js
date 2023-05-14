c750ea8fda52b5e5927821263658dceb402b844d
tineshwar_singh <tineshwar.singh123@gmail.com>
2023-05-14 16:14:44 +0530

    JS_Variables added

---
+++ JavaScript/002-JS_Variables.js
@@ -0,0 +1,31 @@
+//? Intro to Variable :-
+//* Variables are containers for storing data (storing data values).
+//* variables can store some information.
+//* we can use that information later.
+//* we can change that information later.
+
+console.log("--- 002-JS_Variables.js ---");
+
+
+//TODO : declare a variable 
+//? We can declare a JavaScript variable with the "var" keyword:
+var firstName;
+//* After the declaration, the variable has no value (technically it is undefined).
+
+
+//TODO :  assign a value
+firstName = "Tineshwar";
+
+
+//TODO : use a variable 
+console.log(firstName);
+
+
+//TODO : change value 
+firstName = "Raja";
+console.log(firstName);
+
+
+//TODO : We can also assign a value to the variable when you declare it 
+var myName = "Tineshwar Singh";
+
